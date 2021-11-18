1ed5b5c5f8d644f2e48b97b93bfb0c17e3f0a3b8
Yichen Gu <gyichen@gl-login1.arc-ts.umich.edu>
2021-11-17 21:02:18 -0500

    Added OTVAE

--- velovae/model/__init__.py
+++ velovae/model/__init__.py
@@ -1,15 +1,19 @@
 from .VanillaVAE import VanillaVAE
 from .BranchingVAE import BranchingVAE
 from .VAE import VAE
+from .OTVAE import OTVAE
+from .model_util import optimal_transport_duality_gap
 from .velocity import rnaVelocityVanilla, rnaVelocityBranch, rnaVelocityEmbed
 from .TransitionGraph import TransGraph, encodeType
 
 __all__ = [
     "VanillaVAE",
+    "OTVAE",
     "BranchingVAE",
     "VAE",
     "rnaVelocityVanilla",
     "rnaVelocityBranch",
     "rnaVelocityEmbed",
     "TransGraph",
+    "optimal_transport_duality_gap",
     ]
