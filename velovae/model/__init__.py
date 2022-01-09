967810edbb224c411d333221280c365244163484
Yichen Gu <gyichen@gl-login1.arc-ts.umich.edu>
2022-01-09 13:52:07 -0500

    Changed package functions

--- velovae/model/__init__.py
+++ velovae/model/__init__.py
@@ -1,22 +1,27 @@
 from .VanillaVAE import VanillaVAE
 from .VAE import BrVAE
 from .VanillaVAEpp import VanillaVAEpp
-from .model_util import optimal_transport_duality_gap, ode, odeNumpy
-from .velocity import rnaVelocityVanilla, rnaVelocityRhoVAE, rnaVelocityEmbed
+from .model_util import optimal_transport_duality_gap, ode, odeNumpy, odeBr, odeBrNumpy, knnX0, knn_alt, knnX0_random
+from .velocity import rnaVelocityVAE, rnaVelocityBrVAE, rnaVelocityVAEpp, rnaVelocityEmbed
 from .TransitionGraph import TransGraph, encodeType
-from .TrainingData import SCData, SCLabeledData
+from .TrainingData import SCData
 
 __all__ = [
     "VanillaVAE",
     "VanillaVAEpp",
     "BrVAE",
-    "rnaVelocityVanilla",
-    "rnaVelocityRhoVAE",
+    "rnaVelocityVAE",
+    "rnaVelocityBrVAE",
+    "rnaVelocityVAEpp",
     "rnaVelocityEmbed",
     "TransGraph",
     "optimal_transport_duality_gap",
     "ode",
     "odeNumpy",
-    "SCData",
-    "SCLabeledData"
+    "odeBr",
+    "odeBrNumpy",
+    "knnX0",
+    "knn_alt",
+    "knnX0_random",
+    "SCData"
     ]
