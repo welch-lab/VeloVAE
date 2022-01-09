6631d3637eba4b10431520e52143f009ee9665c5
Yichen Gu <gyichen@gl-login1.arc-ts.umich.edu>
2022-01-09 13:49:09 -0500

    Combined SCData and SCLabeledData into a single class

--- velovae/model/TrainingData.py
+++ velovae/model/TrainingData.py
@@ -5,26 +5,6 @@ class SCData(Dataset):
     """
     This is a simple pytorch dataset class for batch training.
     Each sample represents a cell. Each dimension represents a single gene.
-    """
-    def __init__(self, D, weight=None):
-        """
-        D: [N x G] cell by gene data matrix
-        weight: (optional) [N x 1] training weight of each sample
-        """
-        self.M, self.N = D.shape[0], D.shape[1]//2
-        self.data = D
-        self.weight = np.ones((self.M, self.N)) if weight is None else weight
-    
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.weight[idx], idx
-
-class SCLabeledData(Dataset):
-    """
-    This is a simple pytorch dataset class for batch training.
-    Each sample represents a cell. Each dimension represents a single gene.
     THe dataset also contains the cell labels (types).
     """
     def __init__(self, D, labels, weight=None):
@@ -35,7 +15,7 @@ class SCLabeledData(Dataset):
         """
         self.M, self.N = D.shape[0], D.shape[1]//2
         self.data = D
-        self.labels=labels.astype(int)
+        self.labels=labels
         self.weight = np.ones((self.M, self.N)) if weight is None else weight
     
     def __len__(self):
