b48933474f36a1b646b67ad5b33eea9bd316b1da
Yichen Gu <gyichen@gl-login1.arc-ts.umich.edu>
2022-02-20 14:56:41 -0500

    Added initial conditions for batch extraction

--- velovae/model/TrainingData.py
+++ velovae/model/TrainingData.py
@@ -7,7 +7,7 @@ class SCData(Dataset):
     Each sample represents a cell. Each dimension represents a single gene.
     THe dataset also contains the cell labels (types).
     """
-    def __init__(self, D, labels, weight=None):
+    def __init__(self, D, labels, u0=None, s0=None, t0=None, weight=None):
         """
         D: [N x G] cell by gene data matrix
         labels: [N x 1] cell type information
@@ -16,10 +16,15 @@ class SCData(Dataset):
         self.M, self.N = D.shape[0], D.shape[1]//2
         self.data = D
         self.labels=labels
+        self.u0=u0
+        self.s0=s0
+        self.t0=t0
         self.weight = np.ones((self.M, self.N)) if weight is None else weight
     
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
+        if(self.u0 is not None and self.s0 is not None and self.t0 is not None):
+            return self.data[idx], self.labels[idx], self.weight[idx], idx, self.u0[idx], self.s0[idx], self.t0[idx]
         return self.data[idx], self.labels[idx], self.weight[idx], idx
