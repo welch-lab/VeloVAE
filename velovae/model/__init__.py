dd97bc203bacd828c61503e8716afde97cc71fbc
Yichen Gu <gyichen@gl-login2.arc-ts.umich.edu>
2022-03-03 09:42:57 -0500

    Removed unrelated functions

--- velovae/model/__init__.py
+++ velovae/model/__init__.py
@@ -1,24 +1,17 @@
 from .VanillaVAE import VanillaVAE
-#from .VAE import BrVAE
 from .VanillaVAEpp import VanillaVAEpp
-from .model_util import optimal_transport_duality_gap, ode, odeNumpy, odeBr, odeBrNumpy, knnX0, knnX0_alt, knnx0_bin
-from .velocity import rnaVelocityVAE, rnaVelocityBrVAE, rnaVelocityVAEpp, velocity_embedding
-from .TransitionGraph import TransGraph, encodeType
+from .model_util import ode, odeNumpy, knnX0, knnX0_alt, knnx0_bin
+from .velocity import rnaVelocityVAE, rnaVelocityVAEpp, velocity_embedding
 from .TrainingData import SCData
 
 __all__ = [
     "VanillaVAE",
     "VanillaVAEpp",
     "rnaVelocityVAE",
-    "rnaVelocityBrVAE",
     "rnaVelocityVAEpp",
     "velocity_embedding",
-    "TransGraph",
-    "optimal_transport_duality_gap",
     "ode",
     "odeNumpy",
-    "odeBr",
-    "odeBrNumpy",
     "knnX0",
     "knnX0_alt",
     "knnx0_bin",
