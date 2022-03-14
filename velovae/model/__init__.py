96eddcfdf06e4944a1580674bb467fcca0b7c59e
Yichen Gu <gyichen@lh-login2.arc-ts.umich.edu>
2022-03-14 09:33:52 -0400

    Corrected file name

--- velovae/model/__init__.py
+++ velovae/model/__init__.py
@@ -1,19 +1,23 @@
 from .VanillaVAE import VanillaVAE
 from .VAE import VAE
+from .BrODE import BrODE
 from .model_util import ode, odeNumpy, knnX0, knnX0_alt, knnx0_bin
-from .velocity import rnaVelocityVAE, rnaVelocityVAEpp, velocity_embedding
-from .TrainingData import SCData
+from .velocity import rnaVelocityVanillaVAE, rnaVelocityVAE, rnaVelocityBrODE, velocity_embedding
+from .TrainingData import SCData, SCTimedData
 
 __all__ = [
     "VanillaVAE",
     "VAE",
+    "BrODE",
     "rnaVelocityVanillaVAE",
     "rnaVelocityVAE",
+    "rnaVelocityBrODE",
     "velocity_embedding",
     "ode",
     "odeNumpy",
     "knnX0",
     "knnX0_alt",
     "knnx0_bin",
-    "SCData"
+    "SCData",
+    "SCTimedData"
     ]
