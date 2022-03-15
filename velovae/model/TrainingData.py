307ccf85b2b092da38f769990b8ac82b63bd8bcc
Yichen Gu <gyichen@lh-login2.arc-ts.umich.edu>
2022-03-15 10:47:14 -0400

    Added a second dataset class with time (for BrODE)

--- velovae/model/TrainingData.py
+++ velovae/model/TrainingData.py
@@ -13,13 +13,13 @@ class SCData(Dataset):
         labels: [N x 1] cell type information
         weight: (optional) [N x 1] training weight of each sample
         """
-        self.M, self.N = D.shape[0], D.shape[1]//2
+        self.N, self.G = D.shape[0], D.shape[1]//2
         self.data = D
         self.labels=labels
         self.u0=u0
         self.s0=s0
         self.t0=t0
-        self.weight = np.ones((self.M, self.N)) if weight is None else weight
+        self.weight = np.ones((self.N, self.G)) if weight is None else weight
     
     def __len__(self):
         return len(self.data)
@@ -28,3 +28,27 @@ class SCData(Dataset):
         if(self.u0 is not None and self.s0 is not None and self.t0 is not None):
             return self.data[idx], self.labels[idx], self.weight[idx], idx, self.u0[idx], self.s0[idx], self.t0[idx]
         return self.data[idx], self.labels[idx], self.weight[idx], idx
+
+class SCTimedData(Dataset):
+    def __init__(self, D, labels, t, u0=None, s0=None, t0=None, weight=None):
+        """
+        D: [N x G] cell by gene data matrix
+        labels: [N x 1] cell type information
+        weight: (optional) [N x 1] training weight of each sample
+        """
+        self.N, self.G = D.shape[0], D.shape[1]//2
+        self.data = D
+        self.labels=labels
+        self.time = t.reshape(-1,1)
+        self.u0=u0
+        self.s0=s0
+        self.t0=t0
+        self.weight = np.ones((self.N, self.G)) if weight is None else weight
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if(self.u0 is not None and self.s0 is not None and self.t0 is not None):
+            return self.data[idx], self.labels[idx], self.time[idx], self.weight[idx], idx, self.u0[idx], self.s0[idx], self.t0[idx]
+        return self.data[idx], self.labels[idx], self.time[idx], self.weight[idx], idx
