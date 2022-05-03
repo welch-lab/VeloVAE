f325dccef0a9e29af39c79878ffd31409075be8d
Yichen Gu <gyichen@gl-login2.arc-ts.umich.edu>
2022-05-03 14:03:40 -0400

    Latest velocity computation

--- velovae/model/velocity.py
+++ velovae/model/velocity.py
@@ -10,8 +10,31 @@ from .model_util import odeNumpy, ode_br_numpy, predSUNumpy
 
 def rnaVelocityVanillaVAE(adata, key, use_raw=False, use_scv_genes=False, k=10):
     """
+    < description >
     Compute the velocity based on:
     ds/dt = beta * u - gamma * s
+    
+    < Input Arguments >
+    1.  adata [AnnData]
+    
+    2.  key [string]
+        key used for extracting ODE parameters
+    
+    3.  use_raw [bool]
+        whether to use the (noisy) input count to compute the velocity
+    
+    4.  use_scv_genes [bool]
+        whether to compute velocity only for genes scVelo fits
+    
+    < Output >
+    1.  V [array (N x G)]
+        velocity
+    
+    2.  U [array (N x G)]
+        predicted u values
+    
+    3.  S [array (N x G)]
+        predicted s values
     """
     alpha = adata.var[f"{key}_alpha"].to_numpy()
     beta = adata.var[f"{key}_beta"].to_numpy()
@@ -31,7 +54,7 @@ def rnaVelocityVanillaVAE(adata, key, use_raw=False, use_scv_genes=False, k=10):
             adata.layers["Uhat"] = U*scaling
             adata.layers["Shat"] = S
     
-    soft_coeff = 1/(1+np.exp(-(t.reshape(-1,1) - ton)*k))
+    soft_coeff = 1/(1+np.exp(-(t.reshape(-1,1) - ton)*k)) #smooth transition at the switch-on time
     
     V = (beta * U - gamma * S)*soft_coeff
     adata.layers[f"{key}_velocity"] = V
@@ -40,10 +63,50 @@ def rnaVelocityVanillaVAE(adata, key, use_raw=False, use_scv_genes=False, k=10):
         V[:, gene_mask] = np.nan
     return V, U, S
 
-def rnaVelocityBrODE(adata, key, use_raw=False, use_scv_genes=False):
+def rnaVelocityVAE(adata, key, use_raw=False, use_scv_genes=False, sigma=None, approx=False, full_vb=False):
     """
     Compute the velocity based on:
     ds/dt = beta * u - gamma * s
+    """
+    alpha = np.exp(adata.var[f"{key}_logmu_alpha"].to_numpy()) if full_vb else adata.var[f"{key}_alpha"].to_numpy()
+    rho = adata.layers[f"{key}_rho"]
+    beta = np.exp(adata.var[f"{key}_logmu_beta"].to_numpy()) if full_vb else adata.var[f"{key}_beta"].to_numpy()
+    gamma = np.exp(adata.var[f"{key}_logmu_gamma"].to_numpy()) if full_vb else adata.var[f"{key}_gamma"].to_numpy()
+    t = adata.obs[f"{key}_time"].to_numpy()
+    t0 = adata.obs[f"{key}_t0"].to_numpy()
+    U0 = adata.layers[f"{key}_u0"]
+    S0 = adata.layers[f"{key}_s0"]
+    scaling = adata.var[f"{key}_scaling"].to_numpy()
+    if(use_raw):
+        U, S = adata.layers['Mu'], adata.layers['Ms']
+    else:
+        if(f"{key}_uhat" in adata.layers and f"{key}_shat" in adata.layers):
+            U, S = adata.layers[f"{key}_uhat"], adata.layers[f"{key}_shat"]
+            U = U/scaling
+        else:
+            U, S = predSUNumpy(np.clip(t-t0,0,None).reshape(-1,1),U0/scaling,S0,alpha*rho,beta,gamma)
+            U, S = np.clip(U, 0, None), np.clip(S, 0, None)
+            adata.layers["Uhat"] = U * scaling
+            adata.layers["Shat"] = S
+    if(approx):
+        V = (S - S0)/((t - t0).reshape(-1,1))
+    else:
+        V = (beta * U - gamma * S)
+    if(sigma is not None):
+        time_order = np.argsort(t)
+        V[time_order] = gaussian_filter1d(V[time_order], sigma, axis=0, mode="nearest")
+    adata.layers[f"{key}_velocity"] = V
+    if(use_scv_genes):
+        gene_mask = np.isnan(adata.var['fit_scaling'].to_numpy())
+        V[:, gene_mask] = np.nan
+    return V, U, S
+
+def rnaVelocityBrODE(adata, key, use_raw=False, use_scv_genes=False, k=10.0):
+    """
+    < Description >
+    Compute the velocity based on:
+    ds/dt = beta * u - gamma * s where
+    u and s are predicted by branching ODE
     """
     alpha = adata.varm[f"{key}_alpha"].T
     beta = adata.varm[f"{key}_beta"].T
@@ -83,45 +146,8 @@ def rnaVelocityBrODE(adata, key, use_raw=False, use_scv_genes=False):
     
     V = np.zeros(S.shape)
     for i in range(alpha.shape[0]):
-        V[y==i] = (beta[i]*U[y==i] - gamma[i]*S[y==i])
-    adata.layers[f"{key}_velocity"] = V
-    if(use_scv_genes):
-        gene_mask = np.isnan(adata.var['fit_scaling'].to_numpy())
-        V[:, gene_mask] = np.nan
-    return V, U, S
-    
-def rnaVelocityVAE(adata, key, use_raw=False, use_scv_genes=False, sigma=None, approx=False, full_vb=False):
-    """
-    Compute the velocity based on:
-    ds/dt = beta * u - gamma * s
-    """
-    alpha = np.exp(adata.var[f"{key}_logmu_alpha"].to_numpy()) if full_vb else adata.var[f"{key}_alpha"].to_numpy()
-    rho = adata.layers[f"{key}_rho"]
-    beta = np.exp(adata.var[f"{key}_logmu_beta"].to_numpy()) if full_vb else adata.var[f"{key}_beta"].to_numpy()
-    gamma = np.exp(adata.var[f"{key}_logmu_gamma"].to_numpy()) if full_vb else adata.var[f"{key}_gamma"].to_numpy()
-    t = adata.obs[f"{key}_time"].to_numpy()
-    t0 = adata.obs[f"{key}_t0"].to_numpy()
-    U0 = adata.layers[f"{key}_u0"]
-    S0 = adata.layers[f"{key}_s0"]
-    scaling = adata.var[f"{key}_scaling"].to_numpy()
-    if(use_raw):
-        U, S = adata.layers['Mu'], adata.layers['Ms']
-    else:
-        if(f"{key}_uhat" in adata.layers and f"{key}_shat" in adata.layers):
-            U, S = adata.layers[f"{key}_uhat"], adata.layers[f"{key}_shat"]
-            U = U/scaling
-        else:
-            U, S = predSUNumpy(np.clip(t-t0,0,None).reshape(-1,1),U0/scaling,S0,alpha*rho,beta,gamma)
-            U, S = np.clip(U, 0, None), np.clip(S, 0, None)
-            adata.layers["Uhat"] = U * scaling
-            adata.layers["Shat"] = S
-    if(approx):
-        V = (S - S0)/((t - t0).reshape(-1,1))
-    else:
-        V = (beta * U - gamma * S)
-    if(sigma is not None):
-        time_order = np.argsort(t)
-        V[time_order] = gaussian_filter1d(V[time_order], sigma, axis=0, mode="nearest")
+        soft_coeff = 1/(1+np.exp(-(t[y==i].reshape(-1,1) - t_trans[parents[i]])*k)) #smooth transition at the switch-on time
+        V[y==i] = (beta[i]*U[y==i] - gamma[i]*S[y==i]) * soft_coeff
     adata.layers[f"{key}_velocity"] = V
     if(use_scv_genes):
         gene_mask = np.isnan(adata.var['fit_scaling'].to_numpy())
