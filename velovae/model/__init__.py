3cad92cd158a8498b6b7ec60ca1fb8c3cada540a
Yichen Gu <gyichen@gl-login2.arc-ts.umich.edu>
2022-05-03 14:04:19 -0400

    Updated module functions

--- velovae/model/__init__.py
+++ velovae/model/__init__.py
@@ -3,7 +3,7 @@ from .VAE import VAE,VAEFullVB
 from .BrODE import BrODE
 from .model_util import ode, odeNumpy
 from .model_util import knnX0, knnX0_alt, knnx0_bin, knn_transition_prob
-from .model_util import encode_type, str2int, int2str
+from .model_util import encode_type, str2int, int2str, sample_genes
 from .velocity import rnaVelocityVanillaVAE, rnaVelocityVAE, rnaVelocityBrODE, velocity_embedding
 from .TrainingData import SCData, SCTimedData
 from .TransitionGraph import TransGraph, edmond_chu_liu
