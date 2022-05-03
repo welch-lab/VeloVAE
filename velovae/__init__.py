ff46e3fb3164ccbecd9a456a289a07ea5b1c3d3d
Yichen Gu <gyichen@gl-login2.arc-ts.umich.edu>
2022-05-03 13:49:27 -0400

    Updated package functions

--- velovae/__init__.py
+++ velovae/__init__.py
@@ -5,16 +5,20 @@ from .plotting import (plot_sig,
                        plot_phase, 
                        plot_cluster,
                        plot_time, 
+                       plot_time_var,
+                       plot_state_var,
                        plot_train_loss, 
                        plot_test_loss, 
                        plot_test_acc, 
                        plot_phase_grid,
                        plot_sig_grid,
                        plot_time_grid,
-                       plot_cluster_grid,
                        plot_velocity,
                        plot_velocity_stream,
                        plot_cell_trajectory,
-                       plotUmapTransition)
+                       plot_trajectory_3d,
+                       plot_umap_transition,
+                       plot_transition_graph,
+                       plot_transcription_grid)
 from .preprocessing import preprocess
                       
