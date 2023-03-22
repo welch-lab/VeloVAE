138b44c7d2257e682b14b694382ef088b89d0a27
Yichen Gu <gyichen@gl-login3.arc-ts.umich.edu>
2023-03-22 09:16:39 -0400

    added calibrated CBDir

--- velovae/analysis/__init__.py
+++ velovae/analysis/__init__.py
@@ -1,7 +1,8 @@
 from .evaluation import post_analysis
-from .evaluation_util import branch_fit_score
+from .evaluation_util import branch_fit_score, calibrated_cross_boundary_correctness
 
 __all__ = [
     "post_analysis",
-    "branch_fit_score"
+    "branch_fit_score",
+    "calibrated_cross_boundary_correctness"
     ]
