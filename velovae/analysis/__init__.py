431ffec126c100c3786c6a07a19ad2e47c4bd519
Yichen Gu <gyichen@gl-login3.arc-ts.umich.edu>
2023-03-27 16:27:48 -0400

    Updated accessible classes/methods

--- velovae/analysis/__init__.py
+++ velovae/analysis/__init__.py
@@ -1,8 +1,9 @@
 from .evaluation import post_analysis
 from .evaluation_util import branch_fit_score, calibrated_cross_boundary_correctness
-
+from .perf_logger import PerfLogger
 __all__ = [
     "post_analysis",
     "branch_fit_score",
-    "calibrated_cross_boundary_correctness"
+    "calibrated_cross_boundary_correctness",
+    "PerfLogger"
     ]
